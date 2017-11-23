d1b7795c4c1643b2dfac86e1b5ae54443a6450df
Ivan Fan <wow-ivan@hotmail.com>
2017-11-23 15:51:49 +1200

    added ms id and password

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "6979e1f3-4d9b-4e43-b192-5e1699be5900",
+    appPassword: "ywvyPUQMPZ3079]nrcB3}-]"
 });
 
 // Listen for messages from users 
